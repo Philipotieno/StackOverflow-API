107ff39051ee5e2e8566e9a61a88029e349244af
philipotieno <tmitchellb007@gmail.com>
2018-12-16 23:13:33 +0300

    [Start-162680095]Added rute for user reqistration

--- run.py
+++ run.py
@@ -1,12 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+USER = {}
 
+#Home page
 @app.route('/', methods=['GET'])
 def home():
-	return jsonify({'message' : 'welcome to my page'})
+	return jsonify({'message' : 'welcome to my page'}), 200
 
+@app.route('/api/v1/signup', methods=['POST'])
+def signup():
+	name = request.get_json()["name"]
+	username = request.get_json()["username"]
+	email = request.get_json()["email"]
+	password = request.get_json()["password"]
+
+	if username not in USER:
+		USER.update({username:{"name":name, "email":email, "password":password}})
+		return jsonify(USER), 200
+	return jsonify({'message' : 'username already exist'}), 409
 
 if __name__ == '__main__':
 	app.run(debug=True)
