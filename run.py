04f05c81ce5785b49cf7c980a85a1d35e53f8343
philipotieno <tmitchellb007@gmail.com>
2018-12-16 22:10:43 +0300

    [Start-162680095]set up flask

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-run.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+	return jsonify({'message' : 'welcome to my page'})
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
