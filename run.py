cd8a04b5260771317151c55fad6bae9760e50c21
philipotieno <tmitchellb007@gmail.com>
2018-12-17 09:57:07 +0300

    [Start-162680095] Added email validation

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -11,16 +12,20 @@ def home():
 
 @app.route('/api/v1/signup', methods=['POST'])
 def signup():
+
 	data = request.get_json()
 	name = data["name"]
 	username = data["username"]
 	email = data["email"]
 	password = data["password"]
 
-	if username not in USER:
-		USER.update({username:{"name":name, "email":email, "password":password}})
-		return jsonify(USER), 200
-	return jsonify({'message' : 'username already exist'}), 409
+	if re.match("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+[a-zA-Z0-9-.]+$)", email) == None:
+		return jsonify({"message" : "email is invalid"}), 403
+	else:
+		if username not in USER:
+			USER.update({username:{"name":name, "email":email, "password":password}})
+			return jsonify(USER), 200
+		return jsonify({'message' : 'username already exist'}), 409
 
 if __name__ == '__main__':
 	app.run(debug=True)
